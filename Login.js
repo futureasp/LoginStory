64fc372289ad728578b0e0b82ab625a2c1781c6d
PC201701112044\Administrator <liuqianhebei@outlook.com>
2018-03-27 21:52:58 +0800

    username and password

--- Login.js
+++ Login.js
@@ -3,6 +3,8 @@ let whenUser="When: enter user name [admin]";
 let whenPsw="When: enter password [taylor2018]";
 let then="Then: [successful!]";
 
+let loginStory=[given,whenUser,whenPsw,then];
+
 class Story{
     constructor(content){
         this.content=content;
@@ -51,15 +53,30 @@ class Story{
 }
 
 class LoginStory extends Story{
-    constructor(content){
-        this.content=content;
+    constructor(loginStory){
+        super(loginStory);
         let username,password,expected,actual;
         // let username;
         // let password;
         // let expected;
         // let actual;
     }
-    when(message){
-        super.when(message);
+    When(message){
+        super.When(message);
+        if(/enter user name/i.test(message)){
+            this.username=message.match(/\[(.+?)\]/g);
+        }
+        if(/enter password/i.test(message)){
+        this.password=message.match(/\[(.+?)\]/g);
+        }
     }
+    Then(message){
+        super.Then(message);        
+        this.expect=message.match(/\[(.+?)\]/g);
+        
+    } 
+
 }
+
+let objStory=new LoginStory(loginStory);
+objStory.Play();
